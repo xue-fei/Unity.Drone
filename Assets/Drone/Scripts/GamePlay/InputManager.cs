c25e594540fb16be931e7fc0b09e4510ead93ff1
xue-fei <awnuxcvbn@163.com>
2023-07-18 13:33:11 +0800

    凤凰模拟器

--- Assets/Drone/Scripts/GamePlay/InputManager.cs
+++ Assets/Drone/Scripts/GamePlay/InputManager.cs
@@ -16,6 +16,15 @@ namespace Drone.Scripts.GamePlay
         public float Pedals => _pedals;
         public float Throttle => _throttle;
 
+        /// <summary>
+        /// 左轴输入
+        /// </summary>
+        public Vector2 leftAxisOffset;
+        /// <summary>
+        /// 右轴输入
+        /// </summary>
+        public Vector2 rightAxisOffset;
+
         #endregion
 
         #region Main Methods
@@ -23,7 +32,54 @@ namespace Drone.Scripts.GamePlay
         // Update is called once per frame
         void Update()
         {
-        
+            if (!Application.isFocused) return;
+            if (Joystick.current != null)
+            {
+                leftAxisOffset.x = Input.GetAxis("Left_X");
+                leftAxisOffset.y = Input.GetAxis("Left_Y");
+
+                rightAxisOffset.x = Input.GetAxis("Right_X");
+                rightAxisOffset.y = Input.GetAxis("Right_Y");
+
+                byte[] mByte = (byte[])Joystick.current.ReadValueAsObject();
+                if (mByte.Length == 8)
+                {
+                    float offset = Mathf.Clamp((mByte[5] - 120) * 0.01F, -1, 1);
+                    if (Mathf.Abs(offset) < 0.05f)
+                    {
+                        offset = 0;
+                    }
+                    leftAxisOffset.x = offset;
+
+                    offset = Mathf.Clamp((mByte[3] - 120) * 0.01F, -1, 1);
+                    if (Mathf.Abs(offset) <= 0.05f)
+                    {
+                        offset = 0;
+                    }
+                    leftAxisOffset.y = offset;
+
+                    offset = Mathf.Clamp((mByte[1] - 120) * 0.01F, -1, 1);
+                    if (Mathf.Abs(offset) <= 0.05f)
+                    {
+                        offset = 0;
+                    }
+                    rightAxisOffset.x = offset;
+
+                    offset = Mathf.Clamp((mByte[2] - 120) * 0.01F, -1, 1);
+                    if (Mathf.Abs(offset) <= 0.05f)
+                    {
+                        offset = 0;
+                    }
+                    rightAxisOffset.y = offset;
+                }
+
+                _cyclic.x = rightAxisOffset.x;
+                _cyclic.y = rightAxisOffset.y;
+
+                _throttle = leftAxisOffset.y;
+
+                _pedals = leftAxisOffset.x;
+            }
         }
 
         #endregion
@@ -33,7 +89,7 @@ namespace Drone.Scripts.GamePlay
         private void OnCyclic(InputValue value)
         {
             _cyclic = value.Get<Vector2>();
-            
+
         }
 
         private void OnPedals(InputValue value)
@@ -48,4 +104,3 @@ namespace Drone.Scripts.GamePlay
         #endregion
     }
 }
-
